ce997cdc440d30bf610e73eaae2f08f9f555d632
Matan Narkiss <matan@matannarkiss.com>
2022-05-21 11:45:20 +0300

    Update MainPage.vue

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -4,6 +4,9 @@
 </template>
 
 <script setup>
+  import { useStore } from 'vuex'
+  import { computed } from 'vue'
+
   const store = useStore()
   const username = computed(() => store.state.username);
   const authData = store.state.auth;
